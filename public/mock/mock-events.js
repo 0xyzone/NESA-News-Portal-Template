1d0ec2d640029ff020533cd5e346b202c09407bb
Rijan Maharjan <rijanbsccsit2076@prime.edu.np>
2024-07-09 12:25:08 +0545

    modifications to news page/events page | added events-description page

--- public/mock/mock-events.js
+++ public/mock/mock-events.js
@@ -1,20 +1,50 @@
 const mock_events = [
     {
-        event_title: "NATIONAL QUALIFIER FOR 12TH WORLD CHAMPIONSHIP CONCLUDED SUCCESSFULLY BY NESA",
-        post_date: "September 27, 2020",
-        post_by: "nesa Events, IESF",
-        description: ""
+        title: "Nepal Esports Championship: The Ultimate Battle of Champions",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"Valorant",
+        prize_pool: 20000
     },
     {
-        event_title: "NATIONAL QUALIFIER FOR 12TH WORLD CHAMPIONSHIP CONCLUDED SUCCESSFULLY BY NESA",
-        post_date: "September 27, 2020",
-        post_by: "nesa Events, IESF",
-        description: ""
+        title: "Kathmandu Esports Showdown: The Clash of Legends",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"Brawl Stars",
+        prize_pool: 20000
     },
     {
-        event_title: "NATIONAL QUALIFIER FOR 12TH WORLD CHAMPIONSHIP CONCLUDED SUCCESSFULLY BY NESA",
-        post_date: "September 27, 2020",
-        post_by: "nesa Events, IESF",
-        description: ""
+        title: "Nepal Gaming Carnival: A Festival of Esports and Virtual Reality",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"EFootball",
+        prize_pool: 20000
+    },
+    {
+        title: "Mount Everest Esports Cup: Reaching New Heights in Competitive Gaming",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"Valorant",
+        prize_pool: 20000
+    },
+    {
+        title: "Valley of Gamers Esports Battle: The Ultimate Gaming Showdown in Kathmandu",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"Mobile Legends",
+        prize_pool: 20000
+    },
+    {
+        title: "Nepal Pro Gamers Invitational: The Summit of Esports Excellence",
+        start_date: "10.07.24",
+        end_date:"13.07.24",
+        organized_by: "NESA Events, IESF",
+        game:"Valorant",
+        prize_pool: 20000
     },
 ]
